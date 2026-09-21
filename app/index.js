f0e506185f521d82a7dd700c4408602562c32057
Anasdagga <daggaanas0888@gmail.com>
2026-09-21 14:58:33 +0530

    add api and get selectors

--- app/index.js
+++ app/index.js
@@ -1,18 +1,44 @@
-async function fetchData() {
-    try{
-        const dataBtn = document.querySelector('.getDataBtn')
-      
-        const response = await fetch(
-            `75b162da8f9146a493d60222261909`
-        );
-        if(!response.ok){
-            throw new Error("could not fetch data")
-        }
-        const data = await response.json();
-    }
-    catch(error){
-        console.error(error);
-    }
-    
-}
 
+const card = document.querySelector(".weatherCard")
+const searchBtn = document.querySelector(".searchBtn");
+const searchBar = document.querySelector(".searchCity");
+const cityName  = document.querySelector(".city");
+const temp = document.querySelector(".temperature");
+const discriptionWeather = document.querySelector(".discription");
+const apiKey = `e526affb06905071c9b36ceaa2922194`
+const weatherForm = document.querySelector(".searchForm")
+
+weatherForm.addEventListener("submit", async event => {
+event.preventDefault();
+const city = cityName.value;
+if(city){
+try{
+const weatherData = await getWeatherData(city)
+displayWeatherInfo(weatherData);
+
+}
+catch(error){
+console.error(error);
+displayError(error)
+}
+}
+else displayError("please enter a city")
+});
+
+
+async function getWeatherData(cityName){
+    const apiUrl= `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
+    const response = await fetch(apiUrl)
+    const data = response.json()
+console.log(data);
+return data;
+}
+function displayWeatherInfo(data){
+
+}
+function displayError(message){
+const errorDisplay = document.createElement('p')
+errorDisplay.textContent = message;
+card.innerHTML = "";
+card.appendChild(errorDisplay);
+}
