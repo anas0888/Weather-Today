7d61d22524c5b110f1e5ce2916b5fbfe3cb9647a
Anasdagga <daggaanas0888@gmail.com>
2026-09-21 10:53:42 +0530

    remove async fxn

--- app/index.js
+++ app/index.js
@@ -1,3 +1,18 @@
-const myArr = ['anas','adnan','axiom']
+async function fetchData() {
+    try{
+        const dataBtn = document.querySelector('.getDataBtn')
+      
+        const response = await fetch(
+            `75b162da8f9146a493d60222261909`
+        );
+        if(!response.ok){
+            throw new Error("could not fetch data")
+        }
+        const data = await response.json();
+    }
+    catch(error){
+        console.error(error);
+    }
+    
+}
 
-console.log(myArr);
