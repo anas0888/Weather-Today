f5c41c3cf89a7c343b1aa73139f37e4e55b983fe
Anasdagga <daggaanas0888@gmail.com>
2026-09-23 15:44:05 +0530

    throw error if city name is empty

--- app/index.js
+++ app/index.js
@@ -1,14 +1,18 @@
-const searchForm = document.querySelector(".searchForm");
-const searchBtn = document.querySelector(".searchBtn");
-const weatherCard = document.querySelector(".weatherCard");
-const cityName = document.querySelector(".city");
-const weatherTemperature = document.querySelector(".temperature");
-const weatherDescription = document.querySelector(".description");
-const searchCity = document.querySelector(".searchCity");
+    const searchForm = document.querySelector(".searchForm");
+    const searchBtn = document.querySelector(".searchBtn");
+    const weatherCard = document.querySelector(".weatherCard");
+    const cityName = document.querySelector(".city");
+    const weatherTemperature = document.querySelector(".temperature");
+    const weatherDescription = document.querySelector(".description");
+    const searchCity = document.querySelector(".searchCity");
 
-searchForm.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    console.log("form submitted");
-    const city = searchCity.value;
-console.log(city);
-});
+    searchForm.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        console.log("form submitted");
+        if(searchCity.value.trim()== ""){
+            console.error("please enter a city name")
+        }
+        else{
+        const city = searchCity.value.trim();
+    console.log(city);}
+    });
