411da8452925ec1127d06b5258c9ea5bebce5685
Anasdagga <daggaanas0888@gmail.com>
2026-09-23 12:25:11 +0530

    get all the HTML elements

--- app/index.js
+++ app/index.js
@@ -1,2 +1,6 @@
-
-
+const searchForm = document.querySelector(".searchForm");
+const searchBtn = document.querySelector(".searchBtn");
+const weatherCard = document.querySelector(".weatherCard");
+const cityName = document.querySelector(".city");
+const weatherTemperature = document.querySelector(".temperature");
+const weatherDiscription = document.querySelector(".discription");
