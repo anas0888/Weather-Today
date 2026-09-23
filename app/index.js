70c7f0cca9b7a5d0aaffe9baade31ba05cb76fa2
Anasdagga <daggaanas0888@gmail.com>
2026-09-23 15:37:56 +0530

    read city input from user

--- app/index.js
+++ app/index.js
@@ -3,4 +3,12 @@ const searchBtn = document.querySelector(".searchBtn");
 const weatherCard = document.querySelector(".weatherCard");
 const cityName = document.querySelector(".city");
 const weatherTemperature = document.querySelector(".temperature");
-const weatherDiscription = document.querySelector(".discription");
+const weatherDescription = document.querySelector(".description");
+const searchCity = document.querySelector(".searchCity");
+
+searchForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    console.log("form submitted");
+    const city = searchCity.value;
+console.log(city);
+});
